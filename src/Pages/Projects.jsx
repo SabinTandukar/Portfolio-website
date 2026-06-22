71ec4a5e015aaa63c914836fc9ff783a6c3e4fd9
SabinTandukar <sabn.tndkr22@gmail.com>
2026-06-22 17:24:17 +0545

    feat: build project page

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import ProjectCard from "../components/ProjectCard";
+import { projects } from "../data/projects";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div>
+      <h1>Projects</h1>
+
+      {projects.map((project) => (
+        <ProjectCard key={projects.id} project={project} />
+      ))}
+    </div>
+  );
 };
 
 export default Projects;
