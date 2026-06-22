70779a3ddb092dea22b87b7510d7b1c6c6a100be
SabinTandukar <sabn.tndkr22@gmail.com>
2026-06-22 17:07:08 +0545

    feat:create hompage

--- src/three/CarModel.jsx
+++ src/three/CarModel.jsx
@@ -1,15 +1,3 @@
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-
-function Cube() {
-  return (
-    <mesh>
-      <boxGeometry />
-      <meshStandardMaterial />
-    </mesh>
-  );
-}
-
 const CarModel = () => {
   return <div>CarModel</div>;
 };
