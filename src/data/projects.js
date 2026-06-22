afe29eb3fe475a01283085fdd7f60559a0465044
SabinTandukar <sabn.tndkr22@gmail.com>
2026-06-22 17:09:44 +0545

    feat: add projects data

---
+++ src/data/projects.js
@@ -0,0 +1,12 @@
+export const projects = [
+  {
+    id: 1,
+    title: "3D car configurator",
+    tech: ["React", "Three.js"],
+  },
+  {
+    id: 2,
+    title: "E-commerece website",
+    tech: ["React", "Node"],
+  },
+];
