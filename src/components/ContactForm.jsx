0f6cdb46f1640d70c7d05e2a7248936f1e86f451
SabinTandukar <sabn.tndkr22@gmail.com>
2026-06-22 17:27:32 +0545

    feat: create contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { useState } from "react";
 
 const ContactForm = () => {
-  return <div>ContactForm</div>;
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(form);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Name" /> <br />
+      <input type="email" placeholder="Email" /> <br />
+      <textarea placeholder="Message"></textarea>
+      <button type="submit">Send</button>
+    </form>
+  );
 };
 
 export default ContactForm;
