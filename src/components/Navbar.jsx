d192b34edead05c4180423e8e7f4aad92a7bb02e
SabinTandukar <sabn.tndkr22@gmail.com>
2026-06-22 17:01:17 +0545

    feat: create navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <h2>Sabin</h2>
+      <ul>
+        <li>
+          <Link to="">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
