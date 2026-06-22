2845d08431b4d92f00689493bf1a5e61eb63c6a8
SabinTandukar <sabn.tndkr22@gmail.com>
2026-06-22 17:13:33 +0545

    feat: create reusable project card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,7 +1,12 @@
-import React from "react";
-
-const ProjectCard = () => {
-  return <div>ProjectCard</div>;
+const ProjectCard = ({ project }) => {
+  return (
+    <div>
+      <h3>{project.title}</h3>
+      {project.tech.map((item) => (
+        <span key={item}>{item}</span>
+      ))}
+    </div>
+  );
 };
 
 export default ProjectCard;
